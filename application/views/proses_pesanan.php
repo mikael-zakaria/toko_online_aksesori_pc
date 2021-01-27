d2f90ca2a456e7f7586f96815cfbe1011dd9fd29
kentzy988 <67728620+kentzy988@users.noreply.github.com>
2021-01-28 00:24:07 +0700

    M Kentzy Pradana

---
+++ application/views/proses_pesanan.php
@@ -0,0 +1,8 @@
+<div class="container-fluid">
+
+	<!-- Pesan Pembayaran Sukses -->
+	<div class="alert alert-success">
+		<p class="text-center align-middle">Selamat Pesanan Anda Sudah Berhasil</p>
+	</div>
+
+</div>
