6f9678e67d42f6abddb029eca14b760fd102c669
Mika'el Zakaria <57636079+mikael-zakaria@users.noreply.github.com>
2021-01-28 02:54:27 +0700

    Delete application directory

--- application/views/proses_pesanan.php
+++
@@ -1,8 +0,0 @@
-<div class="container-fluid">
-
-	<!-- Pesan Pembayaran Sukses -->
-	<div class="alert alert-success">
-		<p class="text-center align-middle">Selamat Pesanan Anda Sudah Berhasil</p>
-	</div>
-
-</div>
